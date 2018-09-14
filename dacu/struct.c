d5889c944e319058ad35e04dc3452bf5d5359cea
ren <1101553279@qq.com>
2018-09-14 02:39:44 +0000

    add arch.txt

--- dacu/struct.c
+++ dacu/struct.c
@@ -107,3 +107,142 @@ struct dacu{
     u8_t ppt_flag;      // ppt level record
 };
 
+=======================  struct as a whole ======================================
+struct event{
+    u8_t func;          // which function's event
+    union flag{
+        struct{
+            u32_t 
+        };
+    };
+    data
+};
+
+struct event{
+    struct flag flag;
+};
+
+struct timeout{
+    u32_t times[ 10 ];      // record start time for check timeout
+};
+
+union status{
+    u32_t status;           // com / pa / listen functions status
+    struct{
+            
+    };
+};
+
+struct timeout{
+};
+
+struct dacu{
+    struct event e;
+    union status s;
+//for action
+    struct list_head head;  // for storing call wait queue
+    struct timeout out;
+};
+
+----------------------each function : event an status -------------------------------------
+pcom( pecu, pca:button, dacu )
+    event:  
+        network: 
+            pecu:
+                connect 
+                close
+            dacu:
+                accept
+                close
+        
+        pca9539:button
+            accept
+            close
+            
+        timeout  -> none
+
+    status:
+        pidle 
+        pend_wait    
+        phost_using  
+        pother_using 
+
+
+dcom( dacu, pca:uic78, pca:button )
+    event:
+        network: 
+            dacu:
+                connect   dacu7,dacu8
+                accept    dacu7,dacu8
+                close     dacu7,dacu8
+        
+        pca9539:button  
+            connect       connect to end      
+            accept        accept end
+            close         close using
+
+        pca9539:uic78
+            connect       dacu1,dacu2
+            accept        dacu1,dacu2
+            close         dacu1,dacu2
+            
+        timeout
+            connect_timeout
+
+    status:
+       didle
+       dhost_wait 
+       dend_wait
+       dusing
+           dhost_using;    // host be accepted
+           dend_using      // end be accepted
+       dother_using        // current dacu is not in using
+
+broadcast
+    event:
+        network:
+            uic_idle
+
+        pca9539:button  
+            pa_in_start
+            pa_in_stop
+            pa_out_start
+            pa_out_stop
+
+        timeout
+            pa_in_timeout
+            pa_out_timeout
+
+    status:
+        pa_in_idle
+        pa_in_wait
+        pa_in_using
+
+        pa_out_idle
+        pa_out_wait
+        pa_out_using
+
+
+listen( rec(occ) pc:uic56 )
+    event:
+        network:
+            occ_start
+            occ_stop
+
+        pca9539:uic
+            listen_start (uic56)
+            listen_stop  (uic56)
+
+        timeout
+            none
+
+    status:
+        broadcast_idle
+        broadcast_wait
+        broadcast_using
+
+        occ_idle
+        occ_wait
+        occ_using
+
+
